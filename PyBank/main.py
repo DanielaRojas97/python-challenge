3a147f721e7d17f6bf3fb850424a55a245c1397d
DanielaRojas97 <daniela_orb@hotmail.com>
2024-10-21 19:46:52 -0600

    first file added

---
+++ PyBank/main.py
@@ -0,0 +1,69 @@
+import csv
+import os
+
+
+budget_data = os.path.join("..","Data Analytics Bootcamp","Github","python-challenge","PyBank","Resources", "budget_data.csv")  # Input file path
+file_to_output = os.path.join("..","Data Analytics Bootcamp","Github","python-challenge","PyBank","analysis","budget_analysis.txt")  # Output file path
+totalMonths=0
+totalprofit=0
+
+# Add more variables to track other necessary financial data
+
+profitDeltas=[]
+currentDelta=0
+profits=[]
+DeltaSum=0
+AverageChange=0
+MaxValue=0
+MinValue=0
+Dates=[]
+
+#Open file
+with open(budget_data) as PayBankCsv: #Open file
+    PayBankReader = csv.reader(PayBankCsv) #read file
+    header = next(PayBankReader) #skip and Save the header #"["Date","234343"]" 
+
+    for row in PayBankReader: #Row gives back each row in a list form ["Date","234343"]
+        totalMonths=totalMonths+1
+        totalprofit=totalprofit+int(row[1]) 
+        profits.append(int(row[1])) #Create a list of all values in column Profit/Loss
+        Dates.append(row[0])
+    
+
+for i in range(len(profits)): #Loop through which the Profits Deltas will be calculated 
+    if i < (len(profits)-1):
+        currentDelta=profits[i+1]-profits[i]
+        profitDeltas.append(int(currentDelta)) #Create a list to store the Profit Deltas
+    
+for i in range(len(profitDeltas)): #Loop through which we will sum all the Profit Deltas
+    DeltaSum=DeltaSum+profitDeltas[i]
+
+for i in range(len(profitDeltas)-1): #Calculate de greatest increase(max) and the greatest decrease(min)
+    if int(profitDeltas[i]) > MaxValue:
+        MaxValue=int(profitDeltas[i])
+
+    elif int(profitDeltas[i]) < MinValue:
+        MinValue=int(profitDeltas[i])
+
+AverageChange=DeltaSum/(len(profitDeltas))
+
+
+#Resultados
+
+print("Financial Analysis")
+print("------------------------")    
+print(f"Total Months: {totalMonths}")
+print(f"Total Profit: {totalprofit}")
+print(f"Average Change: {AverageChange}")
+print(f"Greatest Increase in Profits: {Dates[(profitDeltas.index(MaxValue))+1]} {MaxValue}")
+print(f"Greatest Decease in Profits: {Dates[(profitDeltas.index(MinValue))+1]} {MinValue}")
+
+with open(file_to_output, "w") as txt_file:
+    txt_file.write("Financial Analysis \n")
+    txt_file.write("------------------------ \n")
+    txt_file.write(f"Total Months: {totalMonths} \n")
+    txt_file.write(f"Total Profit: {totalprofit} \n")
+    txt_file.write(f"Average Change: {AverageChange} \n")
+    txt_file.write(f"Greatest Increase in Profits: {Dates[(profitDeltas.index(MaxValue))+1]} {MaxValue} \n")
+    txt_file.write(f"Greatest Decease in Profits: {Dates[(profitDeltas.index(MinValue))+1]} {MinValue} \n")
+
---
+++ PyPoll/PyPoll2.py
@@ -0,0 +1,72 @@
+import csv
+import os
+
+
+election_data = os.path.join("..","Challenge3","Starter_Code","Starter_Code","PyPoll","Resources", "election_data.csv")  # Input file path
+file_to_output = os.path.join("..","Challenge3","Starter_Code","Starter_Code","PyPoll","Resources","electionResults.txt")  # Output file path
+
+totalVotes=0 #Variable that will help us calculate the total amount of votes
+candidates=[] #All values of the column Candidate will be stored here
+candidateVotes=[] #The total voltes per candidatw will be stored here.
+candidatePercent=[] #All the percetage of votes per each candidate will be stored here
+UniqueCandidates=[] #Unique values foun in the list candidates will be stored here
+candidate_count=0 #The counter of votes that will help us iterate the list candidates and will help us calculate the total votes per candidate
+averageVotes=0 #the variable that will help us calculate the average votes
+
+
+with open(election_data) as ElectionCsv: #Open file
+    ElectionReader = csv.reader(ElectionCsv) #Read file and file type
+    header = next(ElectionReader) #skip and Save the header ['Ballot ID', 'County', 'Candidate'] 
+
+    for row in ElectionReader: #Row gives back each row in a list form ["Date","234343"]
+        totalVotes=totalVotes+1 #Cout all votes
+        candidates.append(row[2]) 
+
+    UniqueCandidates.append(candidates[0]) #Start Unique values. Without it, any different values in the first row vs sencond row would be missed
+
+    for i in range(len(candidates)): #Look for unique values within the list Candiadates
+            if i < (len(candidates)-1):
+                if (candidates[i] != candidates[i+1]) and (candidates[i+1] not in UniqueCandidates):
+                    UniqueCandidates.append(candidates[i+1])
+    
+    for i in UniqueCandidates: #Count the votes per candidates and calculate their average votes
+        candidate_count=0
+        for x in candidates:
+            if i == x:
+                candidate_count=candidate_count+1
+        averageVotes=round((candidate_count/totalVotes)*100,2)
+        
+        candidateVotes.append(candidate_count)
+        candidatePercent.append(averageVotes)
+
+
+summary=list(zip(UniqueCandidates,candidatePercent,candidateVotes)) #build a summary of the information
+
+
+with open(file_to_output, "w") as txt_file: #Save the information in a Txt
+
+    print("ElectionResults")
+    txt_file.write("ElectionResults \n")
+
+    print("---------------------")
+    txt_file.write("--------------------- \n")
+    
+    print(f"Total Votes: {totalVotes}")
+    txt_file.write(f"Total Votes: {totalVotes} \n")
+
+    print("----------------------")
+    txt_file.write("--------------------- \n")
+    
+    for i in range(len(summary)): #iterate through the summary to print all the candidate's information
+        print(f"{summary[i][0]} {summary[i][1]}% ({summary[i][2]})")
+        txt_file.write(f"{summary[i][0]} {summary[i][1]}% ({summary[i][2]}) \n")
+
+    print("----------------------")
+    txt_file.write("---------------------- \n")
+
+    
+    winner=UniqueCandidates[candidateVotes.index(max(candidateVotes))] #look for the max in list candidateVotes to determine the winner
+    print(f"Winner: {winner}")
+    txt_file.write(f"Winner: {winner} \n")
+
+
