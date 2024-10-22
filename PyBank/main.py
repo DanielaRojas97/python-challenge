236b5c432e28eec2ac20fa22ce22c2501c1116ae
DanielaRojas97 <daniela_orb@hotmail.com>
2024-10-21 20:03:58 -0600

    Final Version of the challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,10 @@
 import csv
 import os
 
+from pathlib import Path
 
-budget_data = os.path.join("..","Data Analytics Bootcamp","Github","python-challenge","PyBank","Resources", "budget_data.csv")  # Input file path
-file_to_output = os.path.join("..","Data Analytics Bootcamp","Github","python-challenge","PyBank","analysis","budget_analysis.txt")  # Output file path
+budget_data = Path(__file__).parent / "Resources" / "budget_data.csv" # Input file path
+file_to_output = Path(__file__).parent /"analysis"/"budget_analysis.txt" # Output file path
 totalMonths=0
 totalprofit=0
 
